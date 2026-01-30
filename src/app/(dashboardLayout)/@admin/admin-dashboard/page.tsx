ec52ed64e3e9e4c55b020b4257f55d2b58ee980b
shakhawat <shakhawathossen188@gmail.com>
2026-01-30 09:17:18 +0600

    add dashborad

---
+++ src/app/(dashboardLayout)/@admin/admin-dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AdminDashboardPage() {
+    return (
+        <div>Admin Dashboard Page</div>
+    )
+}
---
+++ src/app/(dashboardLayout)/layout.tsx
@@ -0,0 +1,55 @@
+
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Separator } from "@/components/ui/separator"
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar"
+import React from "react";
+
+export default async function DashboardLayout({ admin, user, provider }: {
+    children: React.ReactNode;
+    admin: React.ReactNode;
+    user: React.ReactNode;
+    provider: React.ReactNode;
+}) {
+    return (
+        <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+                <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <SidebarTrigger className="-ml-1" />
+                    <Separator orientation="vertical" className="mr-2 h-4" />
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="#">
+                                    Building Your Application
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator className="hidden md:block" />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </header>
+                <div className="flex flex-1 flex-col gap-4 p-4">
+                    {/* {children} */}
+                    {admin}
+                    {user}
+                    {provider}
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
